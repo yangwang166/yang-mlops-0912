2470016fe9621e712e123cfbec1a16898d34f3e3
Yang Wang <yangwang166@gmail.com>
2022-09-21 09:14:15 +1000

    test

--- steps/ingest.py
+++ steps/ingest.py
@@ -38,3 +38,5 @@ def load_file_as_dataframe(file_path: str, file_format: str) -> DataFrame:
         return pandas.read_csv(file_path, index_col=0)
     else:
         raise NotImplementedError
+
+# TEST
